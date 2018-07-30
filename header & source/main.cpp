697724dd495e2d0f007d05ea87c25f73f282f7f5
MiriNu <miryamnudelman@gmail.com>
2018-07-30 21:41:54 +0300

    beep

--- header & source/main.cpp
+++ header & source/main.cpp
@@ -7,11 +7,15 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #include "actions.h"
+#include "db.h"
 
 #define NAMESPACES using namespace boost::posix_time;using namespace boost::gregorian;using namespace std;using namespace sql;
 
 NAMESPACES;
 int main(int argc, const char * argv[]) {
-
+	Database& db = Database::getInstance();
+	Connection *con = db.getConnection();
+	
+	showBooks();
 	return 0;
 }
